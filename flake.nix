5d744114dac7895d2aff36558945d0037afc032e
hugosenari <hugosenari@gmail.com>
2022-03-12 01:01:06 -0300

    chore: re index catnip

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = ''Assembler for runtime code generation'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."catnip-main".url = "path:./main";
+  inputs."catnip-main".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."catnip-main".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."catnip-0_1_0".url = "path:./0_1_0";
+  inputs."catnip-0_1_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."catnip-0_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ main/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Assembler for runtime code generation'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-catnip-main.flake = false;
+  inputs.src-catnip-main.owner = "RSDuck";
+  inputs.src-catnip-main.ref   = "refs/heads/main";
+  inputs.src-catnip-main.repo  = "catnip";
+  inputs.src-catnip-main.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-catnip-main"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-catnip-main";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
